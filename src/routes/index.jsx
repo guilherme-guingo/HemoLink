f046c451926a006af07c50394f64ade20e40ac6b
Guilherme Guingo <guilherme.guingo07@gmail.com>
2026-06-06 14:41:34 -0300

    fix-import-naming-StyleGuide

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { DefaultLayout } from '../layouts/DefaultLayout';
 import Home from '../pages/Home';
-import Styleguide from '../pages/Styleguide';
+import Styleguide from '../pages/StyleGuide';
 
 export function AppRoutes() {
   return (
