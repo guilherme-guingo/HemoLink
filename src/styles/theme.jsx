ef3b0a7e5b868108865385f5fcbaee0bf490c84e
Guilherme Guingo <guilherme.guingo07@gmail.com>
2026-06-06 11:42:36 -0300

    first commit

---
+++ src/styles/theme.jsx
@@ -0,0 +1,29 @@
+export const lightTheme = {
+  colors: {
+    primary: '#D90429',
+    secondary: '#EF233C',
+    background: '#EDF2F4',
+    surface: '#FFFFFF',
+    text: '#2B2D42',
+    textLight: '#8D99AE',
+    border: '#DFE2E6',
+    danger: '#D90429',
+    success: '#38B000',
+    hospital: '#4361EE'
+  }
+};
+
+export const darkTheme = {
+  colors: {
+    primary: '#EF233C',
+    secondary: '#D90429',
+    background: '#121212',
+    surface: '#1E1E1E',
+    text: '#EDF2F4',
+    textLight: '#8D99AE',
+    border: '#333333',
+    danger: '#EF233C',
+    success: '#4CC9F0',
+    hospital: '#4361EE'
+  }
+};
