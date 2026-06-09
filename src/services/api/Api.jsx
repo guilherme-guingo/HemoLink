d2297c3524394abd1052af4062421154f3263e0c
IcaroAxis <icaro0013@gmail.com>
2026-06-09 17:56:58 -0300

    feat: adiciona pagina para solicitação de doação

--- src/services/api/Api.jsx
+++ src/services/api/Api.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: 'https://6a271c9ba84f9d39e90846df.mockapi.io/pedidos',
+const api = axios.create({
+  baseURL: 'https://6a271c9ba84f9d39e90846df.mockapi.io',
 });
+
+export default api;
