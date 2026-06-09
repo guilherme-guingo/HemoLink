db721ec169efb53c9f7f2e2c75644e5bfd1ed5cd
x-c-x-c <xcx489743@gmail.com>
2026-06-09 19:40:37 -0300

    corrige campos dos dados na pagina perfil

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,4 +1,5 @@
 import { useFavoritos } from "../../contexts/FavoritesContext";
+import { obterTiposSanguineosCriticos } from "../Catalogo";
 import {
   ContainerPerfil,
   TituloPerfil,
@@ -8,6 +9,7 @@ import {
   NomeHospital,
   EnderecoHospital,
   TipoSanguineo,
+  TiposNecessarios,
   BotaoRemover,
   MensagemVazia,
 } from "./style";
@@ -24,16 +26,36 @@ export const Perfil = () => {
         <MensagemVazia>Você ainda não favoritou nenhum hospital.</MensagemVazia>
       ) : (
         <ListaFavoritos>
-          {favoritos.map((hospital) => (
-            <CardFavorito key={hospital.id}>
-              <NomeHospital>{hospital.nome}</NomeHospital>
-              <EnderecoHospital>📍 {hospital.endereco}</EnderecoHospital>
-              <TipoSanguineo>Necessita: {hospital.sangueNecessario}</TipoSanguineo>
-              <BotaoRemover onClick={() => favoritar(hospital)}>
-                Remover dos favoritos
-              </BotaoRemover>
-            </CardFavorito>
-          ))}
+          {favoritos.map((hospital) => {
+            const totalBlood = Object.values(hospital.bloodStock).reduce(
+              (acc, value) => acc + value,
+              0,
+            );
+            const averageBlood =
+              totalBlood / Object.values(hospital.bloodStock).length;
+            const percentage = Math.min(averageBlood, 100);
+
+            const urgencia =
+              percentage <= 30 ? "Critico" : percentage <= 50 ? "Alerta" : "Regular";
+
+            return (
+              <CardFavorito key={hospital.id}>
+                <NomeHospital>{hospital.name}</NomeHospital>
+                <EnderecoHospital>
+                  📍 {hospital.city} - {hospital.state}
+                </EnderecoHospital>
+                <TipoSanguineo $urgencia={urgencia}>
+                  {urgencia} ({percentage}%)
+                </TipoSanguineo>
+                <TiposNecessarios>
+                  Precisa de: <strong>{obterTiposSanguineosCriticos(hospital.bloodStock)}</strong>
+                </TiposNecessarios>
+                <BotaoRemover onClick={() => favoritar(hospital)}>
+                  Remover dos favoritos
+                </BotaoRemover>
+              </CardFavorito>
+            );
+          })}
         </ListaFavoritos>
       )}
     </ContainerPerfil>
