d57d6ac75456ffa273df313688fbedc9c64656db
Pedro Dayer <pedrob.dayer@gmail.com>
2026-06-10 21:43:05 -0300

    feat: insercao funcao na pagina util + refatoracao de codigos

---
+++ src/util/obterTiposSanguineosCriticos.jsx
@@ -0,0 +1,7 @@
+export function obterTiposSanguineosCriticos(bloodStock) {
+  if (!bloodStock || Object.keys(bloodStock).length === 0) return "Nenhum";
+  const menoresEstoques = Object.entries(bloodStock)
+    .sort((a, b) => a[1] - b[1])
+    .slice(0, 2);
+  return menoresEstoques.map((item) => item[0]).join(", ");
+}
